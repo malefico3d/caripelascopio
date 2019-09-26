5f677001c86173763ab3bcc6af08849508cc9659
Claudio Andaur <malefico3d@gmail.com>
2019-09-26 15:55:06 -0300

    Añadiendo logica para cambiar texturas de ruedas

--- Juego2D.gd
+++ Juego2D.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 # Declare member variables here. Examples:
-# var a = 2
+var ojos_textura = 0
+var caras_textura = 0
+var bocas_textura = 0
 # var b = "text"
-var rotacion_step = 0.0
-var Anim ="none"
+
 
 #var ojos_L = "Cara/Ojos_mask-izquierda/Ojos-izquierda"
 #var ojos_R = "Cara/Ojos_mask-derecha/Ojos-derecha"
@@ -17,12 +18,15 @@ func _ready():
 	get_node("Rotar-ojo-izquierdo").connect("pressed", self,"_on_ojo_izquierdo_pressed")
 	get_node("Rotar-boca").connect("pressed", self, "_on_boca_pressed")
 	get_node("Rotar-cara").connect("pressed", self, "_on_cara_pressed")
+	get_node("PanelContainer/Caras").connect("pressed", self, "_cambiar_textura_caras")
+	get_node("PanelContainer/Ojos").connect("pressed", self, "_cambiar_textura_ojos")
+	get_node("PanelContainer/Bocas").connect("pressed", self, "_cambiar_textura_bocas")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 #	pass
-	print("Cambio")
 	
 		
 func _on_ojo_derecho_pressed():
@@ -38,3 +42,17 @@ func _on_boca_pressed():
 func _on_cara_pressed():
 	print("cara")
 	get_node("Cara/cara_mask/AniCara").play("RotacionCara")
+
+func _cambiar_textura_caras():
+	print("Cambia textura de cara")
+	get_node("PanelContainer/Caras/AnimationPlayer").play("slide")
+
+func _cambiar_textura_ojos():
+	print("Cambia textura de ojos")
+	get_node("PanelContainer/Ojos/AnimationPlayer").play("slide")
+
+func _cambiar_textura_bocas():
+	print("Cambia textura de bocas")
+	get_node("PanelContainer/Bocas/AnimationPlayer").play("slide")
+	
+	
