8621d6a0df8c6be2aa90ab45f1ba4a90019f207f
Claudio Andaur <malefico3d@gmail.com>
2019-09-30 15:07:57 -0300

    Selector de sets de texturas, funcionalidad básica

--- Juego2D.gd
+++ Juego2D.gd
@@ -1,9 +1,20 @@
 extends Node2D
 
 # Declare member variables here. Examples:
-var ojos_textura = 0
-var caras_textura = 0
-var bocas_textura = 0
+var ojos_textura_1 = preload("res://Discos/Ojos_disco1.png")
+var ojos_textura_2 = preload("res://Discos/Ojos_disco2.png")
+var ojos_textura_3 = preload("res://Discos/Ojos_disco3.png")
+var cara_textura_1 = preload("res://Discos/Caras_disco1.png")
+var cara_textura_2 = preload("res://Discos/Caras_disco2.png")
+var cara_textura_3 = preload("res://Discos/Caras_disco3.png")
+var boca_textura_1 = preload("res://Discos/Bocas_disco1.png")
+var boca_textura_2 = preload("res://Discos/Bocas_disco2.png")
+var boca_textura_3 = preload("res://Discos/Bocas_disco3.png")
+
+var bocas_textura = ""
+var C= 0
+var O= 0
+var B= 0
 # var b = "text"
 
 
@@ -18,15 +29,19 @@ func _ready():
 	get_node("Rotar-ojo-izquierdo").connect("pressed", self,"_on_ojo_izquierdo_pressed")
 	get_node("Rotar-boca").connect("pressed", self, "_on_boca_pressed")
 	get_node("Rotar-cara").connect("pressed", self, "_on_cara_pressed")
-	get_node("PanelContainer/Caras").connect("pressed", self, "_cambiar_textura_caras")
-	get_node("PanelContainer/Ojos").connect("pressed", self, "_cambiar_textura_ojos")
-	get_node("PanelContainer/Bocas").connect("pressed", self, "_cambiar_textura_bocas")
+	get_node("Panel/Caras_TexButton").connect("pressed", self, "_on_cara_textura_pressed")
+	get_node("Panel/Ojos_TexButton").connect("pressed", self, "_on_ojos_textura_pressed")
+	get_node("Panel/Bocas_TexButton").connect("pressed", self, "_on_bocas_textura_pressed")
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	pass
+	#pass
+	#print(ojos_textura)
+	#print(caras_textura)
+	#print(bocas_textura)
+	print("----")
 	
 		
 func _on_ojo_derecho_pressed():
@@ -43,16 +58,64 @@ func _on_cara_pressed():
 	print("cara")
 	get_node("Cara/cara_mask/AniCara").play("RotacionCara")
 
+func _on_cara_textura_pressed():
+	get_node("Panel/Caras_TexButton/AnimationPlayer").play("slide")
+	_cambiar_textura_caras()
+	#print(caras_textura)
+	C+=1
+	if C>2:
+		C=0
+func _on_ojos_textura_pressed():
+	print("Cambia textura de ojos")
+	get_node("Panel/Ojos_TexButton/AnimationPlayer").play("slide")
+	_cambiar_textura_ojos()
+	O+=1
+	if O>2:
+		O=0
+func _on_bocas_textura_pressed():
+	print("Cambia textura de bocas")
+	get_node("Panel/Bocas_TexButton/AnimationPlayer").play("slide")
+	_cambiar_textura_bocas()
+	B+=1
+	if B>2:
+		B=0
+
 func _cambiar_textura_caras():
 	print("Cambia textura de cara")
-	get_node("PanelContainer/Caras/AnimationPlayer").play("slide")
+	match C:
+		0:
+			#caras_textura="res:/Discos/Caras_disco1.png"
+			get_node("Cara/cara_mask/AniCara/Caras").set_texture(cara_textura_1)
+		1:
+			get_node("Cara/cara_mask/AniCara/Caras").set_texture(cara_textura_2)
+		2:
+			get_node("Cara/cara_mask/AniCara/Caras").set_texture(cara_textura_3)
 
 func _cambiar_textura_ojos():
 	print("Cambia textura de ojos")
-	get_node("PanelContainer/Ojos/AnimationPlayer").play("slide")
+	match O:
+		0:
+			#caras_textura="res:/Discos/Caras_disco1.png"
+			get_node("Cara/Ojos_mask-derecha/AniGiroOjo/Ojos-derecha").set_texture(ojos_textura_1)
+			get_node("Cara/Ojos_mask-izquierda/AniGiroOjo2/Ojos-izquierda").set_texture(ojos_textura_1)
+		1:
+			get_node("Cara/Ojos_mask-derecha/AniGiroOjo/Ojos-derecha").set_texture(ojos_textura_2)
+			get_node("Cara/Ojos_mask-izquierda/AniGiroOjo2/Ojos-izquierda").set_texture(ojos_textura_2)
+		2:
+			get_node("Cara/Ojos_mask-derecha/AniGiroOjo/Ojos-derecha").set_texture(ojos_textura_3)
+			get_node("Cara/Ojos_mask-izquierda/AniGiroOjo2/Ojos-izquierda").set_texture(ojos_textura_3)
 
 func _cambiar_textura_bocas():
-	print("Cambia textura de bocas")
-	get_node("PanelContainer/Bocas/AnimationPlayer").play("slide")
+	print("Cambia textura de cara")
+	match B:
+		0:
+			#caras_textura="res:/Discos/Caras_disco1.png"
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_1)
+		1:
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_2)
+		2:
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_3)
+	
+
 	
 	
