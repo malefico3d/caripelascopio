8963b4288308d751e5727ecc5faf86271742bb27
Claudio Andaur <malefico3d@gmail.com>
2019-10-01 14:50:59 -0300

    Botones de seleccion de texturas funcionando.

--- Juego2D.gd
+++ Juego2D.gd
@@ -11,7 +11,6 @@ var boca_textura_1 = preload("res://Discos/Bocas_disco1.png")
 var boca_textura_2 = preload("res://Discos/Bocas_disco2.png")
 var boca_textura_3 = preload("res://Discos/Bocas_disco3.png")
 
-var bocas_textura = ""
 var C= 0
 var O= 0
 var B= 0
@@ -29,9 +28,9 @@ func _ready():
 	get_node("Rotar-ojo-izquierdo").connect("pressed", self,"_on_ojo_izquierdo_pressed")
 	get_node("Rotar-boca").connect("pressed", self, "_on_boca_pressed")
 	get_node("Rotar-cara").connect("pressed", self, "_on_cara_pressed")
-	get_node("Panel/Caras_TexButton").connect("pressed", self, "_on_cara_textura_pressed")
-	get_node("Panel/Ojos_TexButton").connect("pressed", self, "_on_ojos_textura_pressed")
-	get_node("Panel/Bocas_TexButton").connect("pressed", self, "_on_bocas_textura_pressed")
+	get_node("Caras_TexButton").connect("pressed", self, "_on_cara_textura_pressed")
+	get_node("Ojos_TexButton").connect("pressed", self, "_on_ojos_textura_pressed")
+	get_node("Bocas_TexButton").connect("pressed", self, "_on_bocas_textura_pressed")
 	
 
 
@@ -59,22 +58,21 @@ func _on_cara_pressed():
 	get_node("Cara/cara_mask/AniCara").play("RotacionCara")
 
 func _on_cara_textura_pressed():
-	get_node("Panel/Caras_TexButton/AnimationPlayer").play("slide")
+	get_node("Caras_TexButton/AnimationPlayer").play("slide")
 	_cambiar_textura_caras()
 	#print(caras_textura)
 	C+=1
 	if C>2:
 		C=0
 func _on_ojos_textura_pressed():
-	print("Cambia textura de ojos")
-	get_node("Panel/Ojos_TexButton/AnimationPlayer").play("slide")
+	get_node("Ojos_TexButton/AnimationPlayer").play("slide")
 	_cambiar_textura_ojos()
 	O+=1
 	if O>2:
 		O=0
 func _on_bocas_textura_pressed():
 	print("Cambia textura de bocas")
-	get_node("Panel/Bocas_TexButton/AnimationPlayer").play("slide")
+	get_node("Bocas_TexButton/AnimationPlayer").play("slide")
 	_cambiar_textura_bocas()
 	B+=1
 	if B>2:
@@ -84,7 +82,6 @@ func _cambiar_textura_caras():
 	print("Cambia textura de cara")
 	match C:
 		0:
-			#caras_textura="res:/Discos/Caras_disco1.png"
 			get_node("Cara/cara_mask/AniCara/Caras").set_texture(cara_textura_1)
 		1:
 			get_node("Cara/cara_mask/AniCara/Caras").set_texture(cara_textura_2)
@@ -93,11 +90,11 @@ func _cambiar_textura_caras():
 
 func _cambiar_textura_ojos():
 	print("Cambia textura de ojos")
+	print("Set para ojos es:", O)
 	match O:
 		0:
-			#caras_textura="res:/Discos/Caras_disco1.png"
 			get_node("Cara/Ojos_mask-derecha/AniGiroOjo/Ojos-derecha").set_texture(ojos_textura_1)
-			get_node("Cara/Ojos_mask-izquierda/AniGiroOjo2/Ojos-izquierda").set_texture(ojos_textura_1)
+			print("ESTO SALE?")
 		1:
 			get_node("Cara/Ojos_mask-derecha/AniGiroOjo/Ojos-derecha").set_texture(ojos_textura_2)
 			get_node("Cara/Ojos_mask-izquierda/AniGiroOjo2/Ojos-izquierda").set_texture(ojos_textura_2)
@@ -109,12 +106,11 @@ func _cambiar_textura_bocas():
 	print("Cambia textura de cara")
 	match B:
 		0:
-			#caras_textura="res:/Discos/Caras_disco1.png"
-			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_1)
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(boca_textura_1)
 		1:
-			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_2)
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(boca_textura_2)
 		2:
-			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(cara_textura_3)
+			get_node("Cara/boca_mask/AniBoca/Bocas").set_texture(boca_textura_3)
 	
 
 	
